bbbe2fe74dfeb8bd3dcbbecb45f55a744e14c7b6
SEUNGWOO LEE <seungwoo.lee@snu.ac.kr>
2023-11-02 18:07:24 +0900

    initial commit

--- sample_scripts/train_eval_dtu.sh
+++ sample_scripts/train_eval_dtu.sh
@@ -7,19 +7,19 @@ project=dtu${num_shots}-$method
 # to overwrite the max_steps in the gin config file, use the following line
 #   --gin_bindings "Config.freq_reg_end = $max_steps" 
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1
 python train.py \
     --gin_configs configs/$method/dtu${num_shots}_${method}.gin \
     --gin_bindings "Config.dtu_scan = 'scan$scan'" \
     --gin_bindings "Config.expname = '$scan-train'" \
-    --gin_bindings "Config.checkpoint_dir = 'out/$method/$project/$scan'" \
+    --gin_bindings "Config.checkpoint_dir = 'train/$method/$project/$scan'" \
     --gin_bindings "Config.project = '$project'" \
-    --gin_bindings "Config.render_chunk_size = 16384" 
+    --gin_bindings "Config.render_chunk_size = 4096" 
 
 python eval.py \
     --gin_configs configs/$method/dtu${num_shots}_${method}.gin \
     --gin_bindings "Config.dtu_scan = 'scan$scan'" \
     --gin_bindings "Config.expname = '$scan-eval'" \
-    --gin_bindings "Config.checkpoint_dir = 'out/$method/$project/$scan'" \
+    --gin_bindings "Config.checkpoint_dir = 'train/$method/$project/$scan'" \
     --gin_bindings "Config.log_img_to_wandb = True" \
     --gin_bindings "Config.project = '$project'"
